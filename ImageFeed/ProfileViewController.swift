a75ba676ebcbfc4cb1072a462e2c45e3acf01dd9
Taisia Galkina <galkinattasya@gmail.com>
2023-09-19 14:15:54 +0300

    Add profile View

---
+++ ImageFeed/ProfileViewController.swift
@@ -0,0 +1,17 @@
+import Foundation
+import UIKit
+
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet private var avatarImageView: UIImageView!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginNameLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+
+    @IBOutlet private var logoutButton: UIButton!
+
+    @IBAction private func didTapLogoutButton() {
+    }
+    
+}
