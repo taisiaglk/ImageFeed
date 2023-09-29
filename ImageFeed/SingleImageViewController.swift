a9ed999189bc3e414d5cc66bf74ba3bf9d64abae
Taisia Galkina <galkinattasya@gmail.com>
2023-09-29 15:42:14 +0300

    Сделано до зума отдельной картинки

--- ImageFeed/SingleImageViewController.swift
+++ ImageFeed/SingleImageViewController.swift
@@ -2,6 +2,22 @@ import Foundation
 import UIKit
 
 class SingleImageViewController: UIViewController {
+    var image: UIImage! {
+        didSet {
+            guard isViewLoaded else { return }
+            imageView.image = image
+        }
+    }
     
     @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+    }
+    
+    @IBAction func didTapBackButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
