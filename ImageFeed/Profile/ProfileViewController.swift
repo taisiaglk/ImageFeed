462bc70cbc2544841adb7f684d1bd3e80c3049b7
Taisia Galkina <galkinattasya@gmail.com>
2023-09-30 17:04:36 +0300

    Добавлен экран профиля пользователя, свёрстанный кодом, и нопка «Поделиться» на экране приложения

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -1,21 +1,112 @@
 import Foundation
 import UIKit
 
-
-class ProfileViewController: UIViewController {
+final class ProfileViewController: UIViewController {
     
-    @IBOutlet private var avatarImageView: UIImageView!
-    @IBOutlet private var nameLabel: UILabel!
-    @IBOutlet private var loginNameLabel: UILabel!
-    @IBOutlet private var descriptionLabel: UILabel!
-
-    @IBOutlet private var logoutButton: UIButton!
-
-    @IBAction private func didTapLogoutButton() {
+    private var nameLabel: UILabel!
+    private var nickLabel: UILabel!
+    private var profileDescription: UILabel!
+    
+    private var profileImageView: UIImageView!
+    private var logOutButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setProfileImage()
+        setNamelabel()
+        setNickName()
+        setDescription()
+        setLogOutButton()
+        
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
-            return .lightContent
-        }
+        return .lightContent
+    }
+    
+    private func setProfileImage() {
+        let profileImageView = UIImageView(image: UIImage(named: "avatar"))
+        profileImageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(profileImageView)
+        profileImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+        profileImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32).isActive = true
+        profileImageView.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        profileImageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        self.profileImageView = profileImageView
+    }
+    
+    private func setNamelabel() {
+        let nameLabel = UILabel()
+        nameLabel.text = "Екатерина Новикова"
+        nameLabel.textColor = .white
+        nameLabel.font = .boldSystemFont(ofSize: 23)
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(nameLabel)
+        nameLabel.leadingAnchor.constraint(equalTo: profileImageView.leadingAnchor).isActive = true
+        nameLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 8).isActive = true
+        nameLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true
+        self.nameLabel = nameLabel
+    }
+    
+    private func setNickName() {
+        let nickLabel = UILabel()
+        nickLabel.text = "@ekaterina_nov"
+        nickLabel.textColor = .gray
+        nickLabel.font = .boldSystemFont(ofSize: 13)
+        nickLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(nickLabel)
+        nickLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor).isActive = true
+        nickLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8).isActive = true
+        nickLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true
+        self.nickLabel = nickLabel
+    }
+    
+    private func setDescription() {
+        let description = UILabel()
+        description.text = "Hello, world!"
+        description.textColor = .white
+        description.font = .boldSystemFont(ofSize: 13)
+        description.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(description)
+        description.leadingAnchor.constraint(equalTo: nickLabel.leadingAnchor).isActive = true
+        description.topAnchor.constraint(equalTo: nickLabel.bottomAnchor, constant: 8).isActive = true
+        description.numberOfLines = 0
+        description.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true
+        self.profileDescription = description
+    }
+    
+    private func setLogOutButton() {
+        let button = UIButton.systemButton(
+            with: UIImage(named: "Exit_button")!,
+            target: self,
+            action: #selector(Self.didTapButton)
+        )
+        button.tintColor = .ypRed
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+        button.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        button.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -24).isActive = true
+        button.centerYAnchor.constraint(equalTo: profileImageView.centerYAnchor).isActive = true
+        self.logOutButton = button
+    }
+    
+    @objc private func didTapButton() {
+        
+        nameLabel?.removeFromSuperview()
+        nameLabel = nil
+        
+        nickLabel?.removeFromSuperview()
+        nickLabel = nil
+        
+        profileDescription?.removeFromSuperview()
+        profileDescription = nil
+        
+        profileImageView?.image = UIImage(systemName: "person.crop.circle.fill")
+        profileImageView?.tintColor = .gray
+    }
+    
     
 }
---
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,61 @@
+import Foundation
+import UIKit
+
+class SingleImageViewController: UIViewController {
+    var image: UIImage! {
+        didSet {
+            guard isViewLoaded else { return }
+            imageView.image = image
+            rescaleAndCenterImageInScrollView(image: image)
+            
+        }
+    }
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+        rescaleAndCenterImageInScrollView(image: image)
+        
+    }
+    
+    @IBAction func didTapBackButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func didTapShareButton(_ sender: UIButton) {
+        let share = UIActivityViewController(
+            activityItems: [image],
+            applicationActivities: nil
+        )
+        present(share, animated: true, completion: nil)
+    }
+    
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, max(hScale, vScale)))
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        let newContentSize = scrollView.contentSize
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+    }
+    
+}
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        imageView
+    }
+}
